599f1c7d9fe8223f55ceef7a5a97a3bc4f7b5564
jonahgasura <u20592061@tuks.co.za>
2023-06-02 22:37:05 +0200

    Added app template

--- AppService/ClientApp/src/app/app.component.ts
+++ AppService/ClientApp/src/app/app.component.ts
@@ -5,5 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  continue() {
+    // Handle continue button click
+  }
+
 }
