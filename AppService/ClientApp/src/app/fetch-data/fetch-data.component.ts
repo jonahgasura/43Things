778a1a65cb286d34666fd009782e1ed29c0a0585
jonahgasura <u20592061@tuks.co.za>
2023-06-03 23:38:21 +0200

    Implemented Sentiment Analysis

--- AppService/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ AppService/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,23 +1,87 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {Goal} from "../goal/goal.model";
 
 @Component({
   selector: 'app-fetch-data',
-  templateUrl: './fetch-data.component.html'
+  templateUrl: './fetch-data.component.html',
+  styleUrls: ['./fetch-data.component.css']
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[] = [];
+   existingGoals: Goal[] = [
+    {
+      id: 1,
+      title: 'Travel the world',
+      description: 'Explore different countries and cultures.',
+      progress: 0,
+      cheers: 0,
+      sentiment: "positive",
+      sentimentValue: 72
+    },
+    {
+      id: 2,
+      title: 'Start a business',
+      description: 'Launch a business within the next year',
+      progress: 0,
+      cheers: 0,
+      sentiment: "positive",
+      sentimentValue: 77
+    },
+    {
+      id: 3,
+      title: 'Get fitter',
+      description: 'Go to the gym at least 3 times a week',
+      progress: 0,
+      cheers: 0,
+      sentiment: "positive",
+      sentimentValue: 69
+    },
+     {
+       id: 4,
+       title: 'Become a bully',
+       description: 'Bully 5 people a day',
+       progress: 0,
+       cheers: 0,
+       sentiment: "negative",
+       sentimentValue: 84
+     },
+     {
+       id: 5,
+       title: 'Vengeance',
+       description: 'Get revenge on all my enemies before the end of this year',
+       progress: 0,
+       cheers: 0,
+       sentiment: "negative",
+       sentimentValue: 81
+     },
+  ];
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
+  hideSentiments: boolean = true;
+  showLoader: boolean = false;
+
+  toggleSentiments() {
+    if(this.hideSentiments) {
+      this.showLoader = true; // Show the loader
+
+      setTimeout(() => {
+        this.hideSentiments = !this.hideSentiments; // Toggle the sentiment visibility after 3 seconds
+        this.showLoader = false; // Hide the loader
+      }, 3000);
+    }
+    else {
+      this.hideSentiments = !this.hideSentiments; // Toggle the sentiment visibility after 3 seconds
+    }
   }
-}
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+  removeGoal(goal: Goal) {
+    const index = this.existingGoals.indexOf(goal);
+    if (index !== -1) {
+      this.existingGoals.splice(index, 1);
+    }
+  }
+
+
+
+
+
 }
